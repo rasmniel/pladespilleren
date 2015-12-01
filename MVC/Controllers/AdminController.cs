5eff14509c9f5acccf203a3c92ca7b9b46c766ba
Rasmus Nielsen <rasmniel@gmail.com>
2015-12-01 13:47:22 +0100

    Added AdminController and temporary views scaffolded

---
+++ MVC/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using BE;
+using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            AdminViewModel model = new AdminViewModel();
+            model.Artists = new List<Artist>();
+            model.Genres = new List<Genre>();
+            return View(model);
+        }
+    }
+}
---
+++ MVC/Models/AdminViewModel.cs
@@ -0,0 +1,15 @@
+﻿using BE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class AdminViewModel
+    {
+        public IEnumerable<Artist> Artists { get; set; }
+
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
