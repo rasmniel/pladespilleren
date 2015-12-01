1655a700e9a35780e2eb56708a0b84c99bffdd47
Rasmus Nielsen <rasmniel@gmail.com>
2015-12-01 12:36:59 +0100

    Added GenreRepository

---
+++ DAL/Repositories/GenreRepository.cs
@@ -0,0 +1,49 @@
+﻿using BE;
+using DAL.Db;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace DAL.Repositories
+{
+    public class GenreRepository : IRepository<Genre>
+    {
+        private readonly PladespillerenDbContext db = new PladespillerenDbContext();
+
+        public Genre Read(int? id)
+        {
+            return db.Genres.FirstOrDefault(genre => genre.Id == id);
+        }
+
+        public IEnumerable<Genre> ReadAll()
+        {
+            return db.Genres.ToList();
+        }
+
+        public bool Create(Genre entity)
+        {
+            db.Genres.Add(entity);
+            db.SaveChanges();
+            return true;
+        }
+
+        public bool Delete(Genre entity)
+        {
+            db.Genres.Remove(entity);
+            db.SaveChanges();
+            return true;
+        }
+
+        public bool Update(Genre entity)
+        {
+            db.Entry(entity).State = EntityState.Modified;
+            db.SaveChanges();
+            return true;
+        }
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+    }
+}
