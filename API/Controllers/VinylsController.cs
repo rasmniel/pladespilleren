2b67824d5b12623d36642ad5b5d3bed2d099a6b5
hardydrachmann <hardydrachmann@phoebuscartel.dk>
2015-12-11 15:25:32 +0100

    added api test solution for vinyls

---
+++ API/Controllers/VinylsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BE;
+using DAL;
+using DAL.Repositories;
+
+namespace API.Controllers
+{
+    public class VinylsController : ApiController
+    {
+        private readonly VinylRepository Repo = DALFacade.GetVinylRepository();
+
+        // GET api/values
+        public HttpResponseMessage Get()
+        {
+            IEnumerable<Vinyl> vinyls = Repo.ReadAll();
+            return Request.CreateResponse(HttpStatusCode.Found, vinyls);
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Tests/VinylsAPITest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using BE;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class VinylsApiTest
+    {
+        HttpClient client;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Get the base address value from App.config
+            string baseAddress = ConfigurationManager.AppSettings["ApiBaseAddress"];
+
+            client = new HttpClient();
+            client.BaseAddress = new Uri(baseAddress);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json")
+            );
+        }
+
+        [Test]
+        public void ReadAllVinylsTest()
+        {
+            HttpResponseMessage response = client.GetAsync("api/vinyls/").Result;
+            var vinyls = response.Content.ReadAsAsync<IEnumerable<Vinyl>>().Result;
+
+            // Test if any vinyls exsists, if not, fail test
+            if (!vinyls.Any())
+            {
+                Assert.Fail();
+            }
+            else
+            {
+                // Test if artist and genre are not null, if not, test successful
+                Vinyl vinyl = vinyls.First();
+                Assert.False(vinyl.Artist == null || vinyl.Genre == null);
+            }
+        }
+    }
+}
