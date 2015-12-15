5482db36a006c2a20f73619fd4558b13b2fd3cad
Rasmus Nielsen <rasmniel@gmail.com>
2015-12-15 13:22:47 +0100

    Extensive makeover of help-pages plus doc

--- API/App_Start/BundleConfig.cs
+++ API/App_Start/BundleConfig.cs
@@ -22,7 +22,7 @@ namespace API
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Main.css"));
         }
     }
 }
